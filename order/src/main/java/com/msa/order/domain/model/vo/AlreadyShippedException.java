ef3960a1a285f284da3e7d04aad70ca91273a4e2
ksw <seonwongim@gmail.com>
2024-10-10 22:25:40 +0900

    feat: 에러 메시지 기능 추가

--- order/src/main/java/com/msa/order/domain/model/vo/AlreadyShippedException.java
+++ order/src/main/java/com/msa/order/domain/model/vo/AlreadyShippedException.java
@@ -1,4 +1,8 @@
 package com.msa.order.domain.model.vo;
 
 public class AlreadyShippedException extends RuntimeException {
+
+    public AlreadyShippedException(String message) {
+        super(message);
+    }
 }
