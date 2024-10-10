c1ea5ed0332c70746dc3eb288ca19c7e18d88b0b
ksw <seonwongim@gmail.com>
2024-10-11 01:23:15 +0900

    update: 1. 주문 생성 반환 DTO @Builder 어노테이션 추가 2. Order 도메인 DTO 정척 매핑 메서드 추가

--- order/src/main/java/com/msa/order/framework/web/dto/response/CreateOrderResponse.java
+++ order/src/main/java/com/msa/order/framework/web/dto/response/CreateOrderResponse.java
@@ -1,13 +1,14 @@
 package com.msa.order.framework.web.dto.response;
 
+import com.msa.order.domain.model.Order;
 import com.msa.order.framework.web.dto.OrderLineDTO;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Getter @Setter
 public class CreateOrderResponse {
 
     private String receiverName;
@@ -16,4 +17,30 @@ public class CreateOrderResponse {
     private String address;
     private List<OrderLineDTO> orderLineDTOList;
     private int totalAmounts;
+
+    @Builder
+    public CreateOrderResponse(String receiverName, String phoneNumber, String zipcode, String address, List<OrderLineDTO> orderLineDTOList, int totalAmounts) {
+        this.receiverName = receiverName;
+        this.phoneNumber = phoneNumber;
+        this.zipcode = zipcode;
+        this.address = address;
+        this.orderLineDTOList = orderLineDTOList;
+        this.totalAmounts = totalAmounts;
+    }
+
+    public static CreateOrderResponse mapToDTO(Order order) {
+
+        List<OrderLineDTO> orderLineDTO = order.getOrderLines().getList().stream()
+                .map(OrderLineDTO::mapToDTO)
+                .toList();
+
+        return CreateOrderResponse.builder()
+                .receiverName(order.getShippingInfo().getReceiverName())
+                .phoneNumber(order.getShippingInfo().getPhoneNumber())
+                .zipcode(order.getShippingInfo().getZipcode())
+                .address(order.getShippingInfo().getAddress())
+                .orderLineDTOList(orderLineDTO)
+                .totalAmounts(order.calculateTotalAmounts().getValue())
+                .build();
+    }
 }
