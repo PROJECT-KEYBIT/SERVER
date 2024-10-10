ce3880e7adb65d8c68dbacebf7327fef22fd127d
ksw <seonwongim@gmail.com>
2024-10-11 00:17:25 +0900

    feat: OrderOutputPort 인터페이스 생성

---
+++ order/src/main/java/com/msa/order/application/outputport/OrderOutputPort.java
@@ -0,0 +1,12 @@
+package com.msa.order.application.outputport;
+
+import com.msa.order.domain.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderOutputPort {
+    Optional<Order> loadOrder(String orderId);
+    Order save(Order order);
+}
