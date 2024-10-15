281e8e3e17edd067c2d5daf1148a3c26b29071b3
ksw <seonwongim@gmail.com>
2024-10-15 11:30:17 +0900

    test: 카테고리 하위 카테고리 추가, 제거 테스트

---
+++ product/src/test/java/com/msa/product/domain/category/model/CategoryTest.java
@@ -0,0 +1,43 @@
+package com.msa.product.domain.category.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CategoryTest {
+
+
+    @DisplayName("[Domain][Category] - 하위 분류 추가 메서드 성공 테스트")
+    @Test
+    void givenSubCategory_whenAddSubCategory_thenWorksFine() {
+        //given
+        Category subCategory = Category.create(1L, "서브 카테고리");
+        Category topCategory = Category.create(2L, "상위 카테고리");
+
+        //when
+        topCategory.addSubCategory(subCategory);
+
+        //then
+        assertThat(topCategory.getSubCategory().contains(subCategory)).isTrue();
+    }
+
+    @DisplayName("[Domain][Category] - 하위 분류 제거 메서드 성공 테스트")
+    @Test
+    void given_when_then() {
+        //given
+        Category subCategory = Category.create(1L, "서브 카테고리");
+        Category topCategory = Category.create(2L, "상위 카테고리");
+        topCategory.addSubCategory(subCategory);
+
+        //when
+        topCategory.removeSubCategory(subCategory);
+
+        //then
+        assertThat(topCategory.getSubCategory().contains(subCategory)).isFalse();
+    }
+
+    public static Category createCategory(Long no, String name) {
+        return Category.create(no, name);
+    }
+}
