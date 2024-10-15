437f0bcc2b4cf2d6f76c9533ace16774b82291b7
ksw <seonwongim@gmail.com>
2024-10-15 13:13:29 +0900

    feature: 상품 상태 추가

--- product/src/main/java/com/msa/product/domain/product/model/Product.java
+++ product/src/main/java/com/msa/product/domain/product/model/Product.java
@@ -12,15 +12,17 @@ import java.util.Set;
 
 @Entity
 @SuperBuilder
-@DiscriminatorColumn @Getter
+@DiscriminatorColumn
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Product {
 
     @EmbeddedId
     private final ProductNo productNo = ProductNo.createProductNo();
 
+    @Getter
     private String name;
 
+    @Getter
     @Embedded
     private Description description;
 
@@ -40,10 +42,55 @@ public abstract class Product {
             joinColumns = @JoinColumn(name = "product_no"))
     private Set<CategoryId> categories = new HashSet<>();
 
+    @Getter
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status = ProductStatus.PREPARING;
+
     protected Product() {}
 
     public void addCategory(CategoryId categoryId) {
-        this.categories.add(categoryId);
+        getCategories().add(categoryId);
+    }
+
+    public void addStock(int stock) {
+        Stock addRequestStock = Stock.create(stock);
+        Stock newAddedStock = getStock().add(addRequestStock);
+        setStock(newAddedStock);
+    }
+
+    public void minusStock(int stock) {
+        Stock minusRequestStock = Stock.create(stock);
+        Stock newMinusedStock = getStock().minus(minusRequestStock);
+        setStock(newMinusedStock);
+    }
+
+    public void preparing() {
+        this.status = ProductStatus.PREPARING;
+    }
+
+    public void sale() {
+        this.status = ProductStatus.SALE;
+    }
+
+    private void soldOut() {
+        this.status = ProductStatus.SOLD_OUT;
+    }
+
+    public void cancel() {
+        this.status = ProductStatus.CANCEL;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    private void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Set<CategoryId> getCategories() {
+        return categories;
     }
 }
 
--- product/src/main/java/com/msa/product/domain/product/model/vo/ProductStatus.java
+++ product/src/main/java/com/msa/product/domain/product/model/vo/ProductStatus.java
@@ -2,5 +2,5 @@ package com.msa.product.domain.product.model.vo;
 
 public enum ProductStatus {
 
-    SALE, SOLD_OUT, CANCEL
+    PREPARING, SALE, SOLD_OUT, CANCEL
 }
