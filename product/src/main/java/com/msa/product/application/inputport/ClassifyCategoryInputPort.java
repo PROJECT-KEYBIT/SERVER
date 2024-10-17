bab149eaa953be40d15ff4cb281efb44b39eb5ab
ksw <seonwongim@gmail.com>
2024-10-18 00:28:55 +0900

    feature: 카테고리 제거 input port 생성

---
+++ product/src/main/java/com/msa/product/application/inputport/ClassifyCategoryInputPort.java
@@ -0,0 +1,29 @@
+package com.msa.product.application.inputport;
+
+import com.msa.product.application.outputport.CategoryOutputPort;
+import com.msa.product.application.usecase.ClassifyCategoryUsecase;
+import com.msa.product.domain.category.model.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ClassifyCategoryInputPort implements ClassifyCategoryUsecase {
+
+    private final CategoryOutputPort outputPort;
+
+    @Override
+    public void removeSubCategory(String categoryNo, String subCategoryNo) {
+        Category category = outputPort.loadCategory(categoryNo)
+                .orElseThrow(() -> new NoSuchElementException(categoryNo + ": 없는 카테고리 아이디입니다."));
+
+        Category subCategory = outputPort.loadCategory(subCategoryNo)
+                .orElseThrow(() -> new NoSuchElementException(subCategoryNo + ": 없는 카테고리 아이디입니다."));
+
+        category.removeSubCategory(subCategory);
+    }
+}
