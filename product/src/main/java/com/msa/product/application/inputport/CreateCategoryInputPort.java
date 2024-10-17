c2a36abeabdf42455263ea6e4594bdd8e7d426af
ksw <seonwongim@gmail.com>
2024-10-18 00:23:18 +0900

    refactor: 카테고리 생성 유스케이스로 변경

--- product/src/main/java/com/msa/product/application/inputport/CreateCategoryInputPort.java
+++ product/src/main/java/com/msa/product/application/inputport/CreateCategoryInputPort.java
@@ -1,10 +1,11 @@
 package com.msa.product.application.inputport;
 
 import com.msa.product.application.outputport.CategoryOutputPort;
-import com.msa.product.application.usecase.AddSubCategoryUsecase;
+import com.msa.product.application.usecase.CreateCategoryUsecase;
 import com.msa.product.domain.category.model.Category;
 import com.msa.product.framework.web.dto.CategoryDTO;
 import com.msa.product.framework.web.dto.request.CreateCategoryRequest;
+import com.msa.product.framework.web.dto.response.CreateCategoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +16,16 @@ import java.util.NoSuchElementException;
 @Repository
 @Transactional
 @RequiredArgsConstructor
-public class AddSubCategoryInputPort implements AddSubCategoryUsecase {
+public class CreateCategoryInputPort implements CreateCategoryUsecase {
 
     private final CategoryOutputPort outputPort;
+
+    @Override
+    public CreateCategoryResponse createCategory(CreateCategoryRequest createCategoryRequest) {
+        Category creatdeCategory = Category.create(createCategoryRequest.name());
+        Category savedCategory = outputPort.save(creatdeCategory);
+        return CreateCategoryResponse.mapToDTO(savedCategory);
+    }
 
     @Override
     public List<CategoryDTO> addSubCategory(String categoryNo, CreateCategoryRequest subCategory) {
--- product/src/main/java/com/msa/product/application/usecase/CreateCategoryUsecase.java
+++ product/src/main/java/com/msa/product/application/usecase/CreateCategoryUsecase.java
@@ -2,10 +2,12 @@ package com.msa.product.application.usecase;
 
 import com.msa.product.framework.web.dto.CategoryDTO;
 import com.msa.product.framework.web.dto.request.CreateCategoryRequest;
+import com.msa.product.framework.web.dto.response.CreateCategoryResponse;
 
 import java.util.List;
 
-public interface AddSubCategoryUsecase {
+public interface CreateCategoryUsecase {
 
+    CreateCategoryResponse createCategory(CreateCategoryRequest createCategoryRequest);
     List<CategoryDTO> addSubCategory(String categoryNo, CreateCategoryRequest subCategory);
 }
