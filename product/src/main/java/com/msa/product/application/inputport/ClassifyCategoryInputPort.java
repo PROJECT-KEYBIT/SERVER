cebdc13f8c0505be0a60bc2fdb79911f5faf62c2
ksw <seonwongim@gmail.com>
2024-10-19 15:00:55 +0900

    update: 서브카테고리 생성 후 추가로 로직 수정

--- product/src/main/java/com/msa/product/application/inputport/ClassifyCategoryInputPort.java
+++ product/src/main/java/com/msa/product/application/inputport/ClassifyCategoryInputPort.java
@@ -4,6 +4,7 @@ import com.msa.product.application.outputport.CategoryOutputPort;
 import com.msa.product.application.usecase.ClassifyCategoryUsecase;
 import com.msa.product.domain.category.model.Category;
 import com.msa.product.framework.web.dto.CategoryDTO;
+import com.msa.product.framework.web.dto.request.CreateCategoryRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +20,7 @@ public class ClassifyCategoryInputPort implements ClassifyCategoryUsecase {
     private final CategoryOutputPort outputPort;
 
     @Override
-    public void removeSubCategory(String categoryNo, String subCategoryNo) {
+    public List<CategoryDTO> removeSubCategory(String categoryNo, String subCategoryNo) {
         Category category = outputPort.loadCategory(categoryNo)
                 .orElseThrow(() -> new NoSuchElementException(categoryNo + ": 없는 카테고리 아이디입니다."));
 
@@ -27,15 +28,18 @@ public class ClassifyCategoryInputPort implements ClassifyCategoryUsecase {
                 .orElseThrow(() -> new NoSuchElementException(subCategoryNo + ": 없는 카테고리 아이디입니다."));
 
         category.removeSubCategory(subCategory);
+
+        return category.getSubCategory().stream()
+                .map(CategoryDTO::mapToDTO)
+                .toList();
     }
 
     @Override
-    public List<CategoryDTO> addSubCategory(String categoryNo, String subCategoryNo) {
+    public List<CategoryDTO> addSubCategory(String categoryNo, CreateCategoryRequest createCategoryRequest) {
         Category category = outputPort.loadCategory(categoryNo)
                 .orElseThrow(() -> new NoSuchElementException(categoryNo + ": 없는 카테고리 아이디입니다."));
 
-        Category subCategory = outputPort.loadCategory(subCategoryNo)
-                .orElseThrow(() -> new NoSuchElementException(categoryNo + ": 없는 카테고리 아이디입니다."));
+        Category subCategory = Category.create(createCategoryRequest.name());
 
         category.addSubCategory(subCategory);
 
