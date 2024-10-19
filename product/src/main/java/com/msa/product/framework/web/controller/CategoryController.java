34b6711aabc1e1c50ab200088125958cf025c860
ksw <seonwongim@gmail.com>
2024-10-19 14:51:32 +0900

    feature: 카테고리 컨트롤러 추가

---
+++ product/src/main/java/com/msa/product/framework/web/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.msa.product.framework.web.controller;
+
+import com.msa.product.application.usecase.ClassifyCategoryUsecase;
+import com.msa.product.application.usecase.CreateCategoryUsecase;
+import com.msa.product.framework.web.dto.CategoryDTO;
+import com.msa.product.framework.web.dto.request.CreateCategoryRequest;
+import com.msa.product.framework.web.dto.response.CreateCategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CategoryController {
+
+    private final CreateCategoryUsecase createCategoryUsecase;
+    private final ClassifyCategoryUsecase classifyCategoryUsecase;
+
+    @PostMapping("/category")
+    public ResponseEntity<CreateCategoryResponse> createCategory(
+            @RequestBody CreateCategoryRequest createCategoryRequest
+    ) {
+        return ResponseEntity.ok(createCategoryUsecase.createCategory(createCategoryRequest));
+    }
+
+    @PostMapping("/category/{categoryNo}/subCategory")
+    public ResponseEntity<List<CategoryDTO>> addSubCategory(
+            @PathVariable String categoryNo,
+            @RequestBody CreateCategoryRequest request
+    ) {
+        return ResponseEntity.ok(classifyCategoryUsecase.addSubCategory(categoryNo, request));
+    }
+
+    @DeleteMapping("/category/{categoryNo}/subCategory/{subCategoryNo}")
+    public ResponseEntity<List<CategoryDTO>> deleteSubCategory(
+            @PathVariable String categoryNo,
+            @PathVariable String subCategoryNo
+    ) {
+        return ResponseEntity.ok(classifyCategoryUsecase.removeSubCategory(categoryNo, subCategoryNo));
+    }
+}
