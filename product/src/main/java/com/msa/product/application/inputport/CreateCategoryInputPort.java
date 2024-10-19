387651b489d70f9b4678264f0c3bb43f7af4b309
ksw <seonwongim@gmail.com>
2024-10-19 15:01:14 +0900

    refactor: 불필요한 의존성 제거

--- product/src/main/java/com/msa/product/application/inputport/CreateCategoryInputPort.java
+++ product/src/main/java/com/msa/product/application/inputport/CreateCategoryInputPort.java
@@ -3,15 +3,11 @@ package com.msa.product.application.inputport;
 import com.msa.product.application.outputport.CategoryOutputPort;
 import com.msa.product.application.usecase.CreateCategoryUsecase;
 import com.msa.product.domain.category.model.Category;
-import com.msa.product.framework.web.dto.CategoryDTO;
 import com.msa.product.framework.web.dto.request.CreateCategoryRequest;
 import com.msa.product.framework.web.dto.response.CreateCategoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @Repository
 @Transactional
