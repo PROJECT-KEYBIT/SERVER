f443f7464640e4ddea0c061200d1ca753c34d3b8
ksw <seonwongim@gmail.com>
2024-10-29 00:14:04 +0900

    feature: 주문 카프카 서비스 빈 등록

--- order/src/main/java/com/msa/order/framework/kafkaadpator/OrderKafkaAdaptor.java
+++ order/src/main/java/com/msa/order/framework/kafkaadpator/OrderKafkaAdaptor.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class OrderKafkaAdaptor implements EventOutputPort {
 
