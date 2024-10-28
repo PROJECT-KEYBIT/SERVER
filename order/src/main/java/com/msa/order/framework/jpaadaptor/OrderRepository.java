94c7d708033907d3e7f618293c6e31c45971704d
ksw <seonwongim@gmail.com>
2024-10-29 00:12:26 +0900

    update: OrderNo 데이터 타입으로 조회 방법 수정

--- order/src/main/java/com/msa/order/framework/jpaadaptor/OrderRepository.java
+++ order/src/main/java/com/msa/order/framework/jpaadaptor/OrderRepository.java
@@ -1,13 +1,14 @@
 package com.msa.order.framework.jpaadaptor;
 
 import com.msa.order.domain.model.Order;
+import com.msa.order.domain.model.vo.OrderNo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
+public interface OrderRepository extends JpaRepository<Order, OrderNo> {
 
     List<Order> findByOrderer_OrdererId(Long ordererId);
 }
