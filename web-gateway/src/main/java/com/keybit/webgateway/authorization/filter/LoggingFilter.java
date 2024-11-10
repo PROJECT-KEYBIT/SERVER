ce4b9d5b9d9407d5d12c447614378c4188475be8
ksw <seonwongim@gmail.com>
2024-11-11 04:56:13 +0900

    feature: 로깅 필터 등록

--- web-gateway/src/main/java/com/keybit/webgateway/authorization/filter/LoggingFilter.java
+++ web-gateway/src/main/java/com/keybit/webgateway/authorization/filter/LoggingFilter.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Slf4j
+@Component
 public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
 
