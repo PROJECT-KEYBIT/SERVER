661640818cbd63e96cc1a0d3cecafc09794f5150
ksw <seonwongim@gmail.com>
2024-11-18 23:17:32 +0900

    feature: @Tag API 추가

--- member/src/main/java/com/keybit/member/framework/web/controller/MemberController.java
+++ member/src/main/java/com/keybit/member/framework/web/controller/MemberController.java
@@ -4,12 +4,14 @@ import com.keybit.member.application.usecase.AddMemberUsecase;
 import com.keybit.member.application.usecase.InquiryMemberUsecase;
 import com.keybit.member.framework.web.dto.MemberDTO;
 import com.keybit.member.framework.web.dto.request.SignUpInfo;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "[회원-API]")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
